df98898189890bcec27a0068fdccc4a77faaa251
Nikola Cywinska <nicywi@gmail.com>
2023-09-16 11:42:06 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Aplikacja zarządzająca studentami';
+  footer = "Copyright 2023 by SDA.";
+}
